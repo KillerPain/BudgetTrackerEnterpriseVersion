fadcb4c2f9f469216be8149e6f30f8dde001d0f6
Ильяс Сабешулы <groza01061995@gmail.com>
2017-12-12 17:00:46 +0600

    new commit

---
+++ src/entities/User.java
@@ -0,0 +1,89 @@
+package entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="people")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id", unique=true, nullable = false)
+    private Long id;
+    
+    @Column(name="username", nullable = true)
+    private String username; 
+    
+    @Column(name="firstname", nullable = true)
+    private String firstname;
+    
+    @Column(name="lastname", nullable = true)
+    private String lastname; 
+
+	@Column(name="email", unique = true, nullable = false)
+    private String email;
+      
+    @Column(name="password", nullable = false)
+    private String password;    
+    
+    public User() {}
+    
+    public User(String email2, String password2) {
+		this.email = email2;
+		this.password = password2;		
+	}
+    
+  	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return username;
+	}
+
+	public void setUserName(String name) {
+		this.username = name;
+	}
+	public String getFirstName() {
+		return firstname;
+	}
+
+	public void setFirstName(String name) {
+		this.firstname = name;
+	}
+
+	public String getLastName() {
+		return lastname;
+	}
+
+	public void setLastName(String name) {
+		this.lastname = name;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+    
+}
